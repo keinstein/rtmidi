dea3914e6c8f59386c48434aa704f8429d1e363e
Tobias Schlemmer <keinstein@users.sourceforge.net>
2018-01-14 14:01:55 +0100

    fix gettext-enabled compilation

--- RtMidi.h
+++ RtMidi.h
@@ -1676,7 +1676,7 @@ namespace rtmidi {
 	public:
 		MidiInDummy( const std::string & /*clientName*/, unsigned int queueSizeLimit )
 			: MidiInApi( queueSizeLimit ) {
-			error( RTMIDI_ERROR(_("MidiInDummy: This class provides no functionality."),
+			error( RTMIDI_ERROR(rtmidi_gettext("MidiInDummy: This class provides no functionality."),
 					    Error::WARNING) );
 		}
 		ApiType getCurrentApi( void ) throw() { return DUMMY; }
@@ -1700,7 +1700,7 @@ namespace rtmidi {
 	{
 	public:
 		MidiOutDummy( const std::string & /*clientName*/ ) {
-			error( RTMIDI_ERROR(_("MidiInDummy: This class provides no functionality."),
+			error( RTMIDI_ERROR(rtmidi_gettext("MidiInDummy: This class provides no functionality."),
 					    Error::WARNING) );
 		}
 		ApiType getCurrentApi( void ) { return DUMMY; }
